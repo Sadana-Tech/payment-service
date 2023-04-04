eeef5773bcd73a57514402fc660eeb4f47e761e0
gauravraw <40800835+gauravraw@users.noreply.github.com>
2023-04-04 12:19:57 +0530

    initial commit

--- src/main/java/com/example/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/example/paymentservice/PaymentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.sadana.payment;
+package com.example.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/paymentservice/PaymentServiceApplicationTests.java
+++ src/test/java/com/example/paymentservice/PaymentServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sadana.payment;
+package com.example.paymentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
